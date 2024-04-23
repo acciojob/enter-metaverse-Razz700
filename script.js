ba63f109d6590517b0ea20e998f2e61394eb32a3
Razz700 <160166041+Razz700@users.noreply.github.com>
2024-04-23 09:58:09 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 //your JS code here. If required.
+let body=document.getElementById("body");
+let p1=document.createElement("p");
+p1.setAttribute("id","status");
+p1.textContent="Enter the Metaverse";
+body.appendChild(p1);
+let btn=document.createElement("button");
+btn.textContent="Enter";
+btn.setAttribute("id","enterBtn");
+body.appendChild(btn);
+btn.addEventListener("click",myfunc);
+function myfunc(){
+	p1.innerHTML="<h1>Entered Metaverse</h1>";
+}
